e0329f6a8678cf4c54eafae911eadd5b09703f3f
sachin <sachingowda@gmail.com>
2024-04-25 15:32:32 +0530

    message

---
+++ app.java
@@ -0,0 +1,12 @@
+package myapp;
+
+public class app {
+	<html>
+	<body>
+			<title>
+				"hi good morning have a nice day"
+			</title>
+	</body>
+</html>
+
+}
---
+++ my.java
@@ -0,0 +1,16 @@
+package myapp;
+
+public class my {
+	<html>
+	<body>
+			<title>
+				"hi good morning"
+				"How are you"
+				" This is java file"
+				"well,good"
+			</title>
+	</body>
+</html>
+
+
+}
